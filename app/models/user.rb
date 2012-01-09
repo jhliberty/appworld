90d7605e37899db614e4a0c4b21fc2cde143e8ed
John-Henry Liberty <johnhenry.liberty@gmail.com>
2012-01-09 00:01:14 -0600

    added user validations and tests

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,17 @@
 class User < ActiveRecord::Base
   authenticates_with_sorcery!
+  
+   attr_accessible :email, :password, :password_confirmation, :username
+   
+   validates_confirmation_of   :password
+   validates_presence_of       :password, :on => :create
+   validates_uniqueness_of     :username
+   validates_format_of         :email, :with => /^[-a-z0-9_+\.]+\@([-a-z0-9]+\.)+[a-z0-9]{2,4}$/i
+   
+   validates :email, :uniqueness => { :case_sensitive => false },
+                     :presence => true
+                     
+   validates :username,  :uniqueness => { :case_sensitive => false },
+                     :presence => true
+  
 end
